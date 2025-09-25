9ca6e109bab4a6c19167835c470c8ac269a84f56
Yunus Emre Kucuk <yunusemrekucuk0053@gmail.com>
2025-09-25 22:18:41 +0300

    v1

---
+++ src/main/java/Runner.java
@@ -0,0 +1,10 @@
+public class Runner {
+    public static void main(String[] args) {
+
+        //Verison 1
+        System.out.println("Hello GIT");
+        System.out.println("git add . : Staging area'ya alır.");
+
+
+    }
+}
